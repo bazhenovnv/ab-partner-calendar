9538ebd48ef8bb657a2bb0b1f063ab64018a181a
bazhenovnv <bazhenniko@gmail.com>
2026-05-05 12:54:17 +0300

    fix: prisma imports final

--- apps/backend/src/modules/dashboard/dashboard.controller.ts
+++ apps/backend/src/modules/dashboard/dashboard.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from '../services/prisma.service';
 
 @Controller('dashboard')
 export class DashboardController {
